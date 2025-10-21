a75c650b48afec6144d8e228acc12906a9851a0d
Scotty-Scott-1 <swilliamfr@outlook.com>
2025-10-21 11:50:52 +0200

    code clean up

--- backend/api/Security/verifyTokenFrontend.js
+++ backend/api/Security/verifyTokenFrontend.js
@@ -2,10 +2,9 @@ const express = require("express");
 const jwt = require("jsonwebtoken");
 const router = express.Router();
 
-/*Receives an access token from react and verifies it with secret key*/
 router.post("/frontend", (req, res) => {
   const { token } = req.body;
-  /*If no token is provided, respond with 401 Unauthorized*/
+
   if (!token) {
     return res.status(403).json({ valid: false, message: "No token provided" });
   }
@@ -13,15 +12,8 @@ router.post("/frontend", (req, res) => {
   console.log("Checking if .env is accessible...");
   if (!process.env.JWT_SECRET) {
     console.log(".env is not available")
-  } else {
-    console.log(!process.env.JWT_SECRE);
   }
 
-
-
-
-
-  /*Verify the token using the secret key*/
   try {
     const decoded = jwt.verify(token, process.env.JWT_SECRET);
     console.log("Decoded token:", decoded);
